b6060b2989287c9771fbf3bd5141f4530f0731ad
Leah <leah.miller@processmaker.com>
2018-07-18 15:12:46 -0700

    event modifier moved to form so it applies to all inputs.  referred to original DOM event targeting the name tag

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -12,16 +12,16 @@
 <body>
     <div id="app" class="container">
         @include ('projects.list')
-        <form method="POST" action="/projects" @submit.prevent="onSubmit">
+        <form method="POST" action="/projects" @submit.prevent="onSubmit" @keydown="errors.clear($event.target.name)"> 
             <div class="control">
                 <label for="name" class="label">Project Name:</label>
-                <input type="text" id="name" name="name" class="input" v-model="name" @keydown="errors.clear('name')">
+                <input type="text" id="name" name="name" class="input" v-model="name">
                 <span class="help is-danger" v-text="errors.get('name')"></span>
             </div>
 
             <div class="control">
                 <label for="description" class="label">Project Description:</label>
-                <input type="text" id="description" name="description" class="input" v-model="description" @keydown="errors.clear('description')">
+                <input type="text" id="description" name="description" class="input" v-model="description">
                 <span class="help is-danger" v-text="errors.get('description')"></span>
             </div>
 
